e38b4a6ea6ca7d35781a32f904f397c59ef8e156
theNareshofficial <thenareshofficial@gmail.com>
2023-09-24 09:16:42 +0530

    Header page

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/header.css">
+        <script src="https://kit.fontawesome.com/3139ed0cde.js" crossorigin="anonymous"></script>
+        <style>
+</style>
+        <title>Hader</title>
+</head>
+<body>
+        <div class="header">
+                <div class="header_left">
+                        <a href="index.php">The_Naresh_official</a>
+                </div>  
+                <div class="header_right">
+                        <a href="index.php">Home</a>
+                        <a href="skill.php">Skill</a>
+                        <a href="project.php">Project</a>
+                        <a href="coffee.php" class="coffee">Coffee <img src="assets/coffee.svg" alt="No Image"></a>
+                </div>
+                <div class="menu">
+                        <i class="fa-solid fa-xmark fa-spin fa-xl" id="close"></i>
+                        <i class="fa-solid fa-bars fa-shake fa-xl" id="open"></i>
+                </div>
+        </div>
+</body>
+</html>

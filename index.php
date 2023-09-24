693981950e906bbd58beb9cc769c700d8de43786
theNareshofficial <thenareshofficial@gmail.com>
2023-09-24 09:17:33 +0530

    Index page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/index.css">
+        <script src="js/link.js"></script>
+        <script src="js/auto_type.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12/dist/typed.umd.min.js"></script>
+        <style>
+                @import url('https://fonts.googleapis.com/css2?family=Handjet:wght@400;500;600&display=swap');
+        </style>
+        <title>Home</title>
+</head>
+
+<?php include("header.php"); ?>
+
+<body>
+        <div class="card">
+                <div class="content">
+                        <h1>It's Me <span>NARESH</span></h1>
+                        <h2>I'm a <span  class="auto-text"> Fullstack Developer</span></h2>
+                        <p>
+                                My programming journey began when I was <span>17</span> years old, and since then I have explored various programming languages, frameworks, and libraries. I have developed many CLI tools and web projects for both the Windows and Linux operating systems, using different technologies and paradigms. Now I'm ready to take on a new challenge: diving deep into the network layer of real-world ethical hacking. I want to learn how to analyze, manipulate, and secure network traffic, as well as how to exploit vulnerabilities and perform penetration testing.
+                        </p>
+                        
+                        <?php include('media.php') ?>
+
+
+                        
+                </div>
+                <div class="profile">
+                        <img src="assets/naresh_4k.png" width="500px" height="100%" alt="No Image">
+                </div>
+                
+        </div>
+        
+</body>
+</html>
+
+<?php include 'footwear.php' ?>
+

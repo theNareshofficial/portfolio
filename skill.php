df6a1914f6274a6da148d3f10d2e0e831f113fa4
theNareshofficial <thenareshofficial@gmail.com>
2023-09-24 09:19:44 +0530

    Skill page

---
+++ skill.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/skill.css">
+        `
+        <title>Skill</title>
+</head>
+
+<?php include ("header.php"); ?>
+
+<body>
+        <div class="container1">
+                <div class="programming">
+                        <h1>Programming Skills</h1>
+                        <h3>C</h3>
+                        <progress value="50" max="100"></progress>
+                        <h3>C++</h3>
+                        <progress value="30" max="100"></progress>
+                        <h3>C#</h3>
+                        <progress value="30" max="100"></progress>
+                        <h3>CSS</h3>
+                        <progress value="90" max="100"></progress>
+                        <h3>HTML 5</h3>
+                        <progress value="95" max="100"></progress>
+                        <h3>Java</h3>
+                        <progress value="60" max="100"></progress>
+                        <h3>Javascript</h3>
+                        <progress value="60" max="100"></progress>
+                        <h3>Python</h3>
+                        <progress value="90" max="100"></progress>
+                        <h3>PHP</h3>
+                        <progress value="90" max="100"></progress>
+                        <h3>Shell</h3>
+                        <progress value="30" max="100"></progress>
+                </div>
+                <div class="framework">
+                        <h1>Framework Skills</h1>
+                        <h3>Flask</h3>
+                        <progress value="50" max="100"></progress>
+                        <h3>Material UI</h3>
+                        <progress value="50" max="100"></progress>
+                        <h3>React JS</h3>
+                        <progress value="70" max="100"></progress>
+                        <h3>Tailwind CSS</h3>
+                        <progress value="50" max="100"></progress>
+                        <h3>Npm</h3>
+                        <progress value="90" max="100"></progress>
+                        <h3>Yarn</h3>
+                        <progress value="90" max="100"></progress>
+                        <h3>GitHub</h3>
+                        <progress value="60" max="100"></progress>
+                        <h3>Git</h3>
+                        <progress value="50" max="100"></progress>
+
+                </div>
+        </div>
+        <div class="container2">
+                <div class="database">
+                        <h1>Database Skills</h1>
+                        <h3>MySQL</h3>
+                        <progress value="70" max="100"></progress>
+                        <h3>MsSQL</h3>
+                        <progress value="70" max="100"></progress>
+                        <h3>MangoDB</h3>
+                        <progress value="30" max="100"></progress>
+                </div>
+                <div class="iot">
+                        <h1>IOT Skills</h1>
+                        <h3>Arduino</h3>
+                        <progress value="10" max="100"></progress>
+                        <h3>Raspberry PI</h3>
+                        <progress value="30" max="100"></progress>
+                </div>
+
+        </div>
+        <div class="container3">
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/twitter.svg" alt="No Image"></a>
+        </div>
+</body>
+</html>
+
+<?php include("footwear.php") ?>
+

7aeebeb8862a297b066291bf05abe1ab6a9e27f5
theNareshofficial <thenareshofficial@gmail.com>
2023-09-24 09:19:04 +0530

    Project page page

---
+++ project.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/project.css">
+        <title>Project</title>
+</head>
+
+<?php include("header.php"); ?>
+
+<body>
+
+
+<h1>Co<span>mmm</span>ing S<span>ooooo</span>n</h1>
+
+<!--
+        <div class="container">
+                <div class="row1">
+                        <div class="project">
+                                <a href="#">
+                                        <h1>Todo App</h1>
+                                        <hr color="orangered"/>
+                                        <h3>HTML & CSS</h3>
+                                </a>
+                        </div>
+                        <div class="project">
+                                <h1>Todo App</h1>
+                                <hr color="orangered"/>
+                                <h3>HTML & CSS</h3>
+                        </div>
+                        <div class="project">
+                                <h1>Todo App</h1>
+                                <hr color="orangered"/>
+                                <h3>HTML & CSS</h3>
+                        </div>
+                </div>
+                
+                <div class="row2">
+                        <div class="project">
+                                <h1>Todo App</h1>
+                                <hr color="orangered"/>
+                                <h3>HTML & CSS</h3>
+                        </div>
+                        <div class="project">
+                                <h1>project2</h1>
+                        </div>
+                        <div class="project">
+                                <h1>project3</h1>
+                        </div>
+                </div>
+                
+                <div class="row3">
+                        <div class="project">
+                             <h1>project1</h1>
+                        </div>
+                        <div class="project">
+                                <h1>project2</h1>
+                        </div>
+                        <div class="project">
+                                <h1>project3</h1>
+                        </div>
+                </div>
+                
+        </div>
+
+-->
+</body>
+</html>
+
+<?php include("footwear.php") ?>

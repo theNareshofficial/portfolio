ce1fe2a028a4ed7b125dd596bc40f7af145d06ce
theNareshofficial <thenareshofficial@gmail.com>
2023-09-24 09:18:13 +0530

    Media page

---
+++ media.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/media.css">
+        <title>media</title>
+</head>
+<body>
+
+        <div class="media">
+                <a href="https://www.instagram.com/the_naresh_offcial/"><img src="assets/instagram.svg" alt="No Image"></a>
+                <a href="https://twitter.com/the_Naresh_tech"><img src="assets/twitter.svg" alt="No Image"></a>
+                <a href="https://github.com/theNareshofficial"><img src="assets/github.svg" alt="No Image"></a>
+                <a href="https://www.threads.net/@the_naresh_offcial"><img src="assets/at-solid.svg" alt="No Image"></a>
+                <a href="https://www.youtube.com/@nareshtechweb930/featured"><img src="assets/youtube.svg" alt="No Image"></a>
+                <a href="#"><img src="assets/google.svg" alt="No image"></a>
+                <a href="coffee.php"><img src="assets/envelope-solid.svg" alt="No Image"></a>
+        </div>
+        <div class="read_more">
+                        <button onclick="read_more()">Read More</button>
+        </div>
+
+</body>
+</html>

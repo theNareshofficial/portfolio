f160e6a32ae81d461a6da1b3a2c25c7469910238
theNareshofficial <thenareshoffcial@gmail.com>
2024-11-25 12:38:42 +0530

    comming soon

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+            <div className="container bg-black p-56">
+              <h1 className='font-bold text-6xl animate-bounce text-red-600 hover:first-letter:text-yellow-500'>Commming Sooon...</h1>
+            </div>
+    </>
+  )
+}
+
+export default Contact

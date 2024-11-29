027fd890b60dc9d5ddd477a5d327f60e15a288ed
theNareshofficial <thenareshoffcial@gmail.com>
2024-11-29 15:12:42 +0530

    screen

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-            <div className="container bg-black p-56">
+            <div className="container w-full h-screen bg-black p-56">
               <h1 className='font-bold text-6xl animate-bounce text-red-600 hover:first-letter:text-yellow-500'>Commming Sooon...</h1>
             </div>
     </>
